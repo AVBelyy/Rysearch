ebb36758f7d630e76c0a138fd8e060c4e1737286
Anton Belyy <anton.belyy@gmail.com>
2017-03-01 01:38:27 +0300

    Implement basic server-side support for ARTM recommender system (with Hellinger distance)

--- server/artm_bridge.py
+++ server/artm_bridge.py
@@ -3,11 +3,20 @@ import pickle
 import json
 import zmq
 
+from sklearn.metrics.pairwise import pairwise_distances
+from pymongo import MongoClient
+from scipy.linalg import norm
+
 MODEL_PATH = "hartm.mdl"
 ZMQ_PORT = 2411
 
 EDGE_THRESHOLD = 0.05
 TOP_N_WORDS = 3
+TOP_N_REC_DOCS = 10
+
+# Initialize MongoDB
+mongo_client = MongoClient()
+db = mongo_client["datasets"]
 
 # Initialize ARTM data
 artm_model = pickle.load(open(MODEL_PATH, "rb"))
@@ -23,15 +32,20 @@ phis = list(map(lambda p: p[1], sorted(phis)))
 topics = {}
 t = lambda lid, tid: "level_%d_%s" % (lid, tid)
 
+# Change this constants if model changes
+[T1, T2, T3] = [10, 30, 70]
+# TODO: index sorting may not be neccessary when we support multiple collections
+rec_theta = artm_model["theta"][:T1].T.sort_index()
+
 # Create topic names
 for lid, phi in enumerate(phis):
     names = phi.index[phi.values.argsort(axis=0)[-TOP_N_WORDS:][::-1].T]
     for tid, top_words in zip(phi.columns, names):
         topics[t(lid, tid)] = {
-            "level_id": lid,
+            "level_id":  lid,
             "top_words": list(top_words),
-            "parents": [],
-            "children": [],
+            "parents":   [],
+            "children":  [],
         }
 
 # Collect topic edges
@@ -51,6 +65,9 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:%d" % ZMQ_PORT)
 
+def hellinger_dist(p, q):
+    return norm(np.sqrt(p) - np.sqrt(q))
+
 print("Start serving ZeroMQ queries on port", ZMQ_PORT)
 
 while True:
@@ -61,9 +78,33 @@ while True:
     # Process query
     if message["act"] == "get_topics":
         response = topics
-    elif message["act"] == "recommend_docs":
-        pass
+    elif message["act"] == "get_recommendations":
+        # TODO: this only works with a single collection of documents (Postnauka) now
+        # TODO: make appropriate fixes when we support multiple collections
+        doc_id = int(message["doc_id"])
+        if doc_id not in rec_theta.index:
+            response = "Unknown `doc_id`"
+        else:
+            dist = pairwise_distances([rec_theta.loc[doc_id]], rec_theta, hellinger_dist)[0]
+            indices = np.argsort(dist)[1:TOP_N_REC_DOCS + 1]
+            # TODO: fix when we support multiple collections
+            sim_docs_ids = list(map(lambda doc_id: "pn_%d" % (doc_id + 1), indices))
+            fields = {"title": 1, "markdown": 1}
+            result = db.postnauka.find({"_id": {"$in": sim_docs_ids}}, fields)
+            result_map = dict(map(lambda v: (v["_id"], v), result))
+            response = []
+            for sim_doc_id in sim_docs_ids:
+                sim_doc = result_map[sim_doc_id]
+                response.append({
+                    "doc_id":   sim_doc["_id"],
+                    "title":    sim_doc["title"],
+                    "markdown": sim_doc["markdown"]
+                })
     else:
         response = "Unknown query"
 
-    socket.send_string(json.dumps({"act": message["act"], "data": response}))
+    socket.send_string(json.dumps({
+        "act":  message["act"],
+        "id":   message.get("id"),
+        "data": response
+    }))
--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var zmq = require("zmq");
+var uuidV4 = require("uuid/v4");
+
+// Initialize common data structures
+
+// TODO: think about using TTL (time-to-live) later
+var routing_queue = {};
 
 // Initialize zmq
 var sock = zmq.socket("req");
@@ -11,16 +17,20 @@ sock.on("message", function (reply) {
     reply = JSON.parse(reply);
     if (reply.act == "get_topics") {
         artm_topics = reply.data;
+    } else if (reply.act == "get_recommendations") {
+        var res = routing_queue[reply.id];
+        delete routing_queue[reply.id];
+        res.send(reply.data);
     }
 });
 
-sock.send("{\"act\":\"get_topics\"}");
+sock.send(JSON.stringify({"act": "get_topics"}));
 
 // Initialize express
 var app = express();
 app.use(express.static("static"));
 
-app.get("/get-topics", function(req, res) {
+app.get("/get-topics", function (req, res) {
     if (artm_topics) {
         res.send(artm_topics);
     } else {
@@ -29,7 +39,18 @@ app.get("/get-topics", function(req, res) {
     }
 });
 
-var server = app.listen(3000, function() {
+app.get("/get-recommendations", function (req, res) {
+    var doc_id = req.query.doc_id;
+    var uuid = uuidV4();
+    routing_queue[uuid] = res;
+    sock.send(JSON.stringify({
+        "act": "get_recommendations",
+        "doc_id": doc_id,
+        "id": uuid,
+    }));
+});
+
+var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
