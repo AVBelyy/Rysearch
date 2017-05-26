fccfc5fe9037c9f2126bd3f748134864902c584a
Anton Belyy <anton.belyy@gmail.com>
2017-05-26 08:19:18 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,12 @@
-with import <nixpkgs> {}; {
-  esenin = stdenv.mkDerivation rec {
+with import <nixpkgs> {};
+
+let bigartm_python = callPackage ./bigartm_py.nix {
+      python27Packages = python27Packages;
+      python35Packages = python35Packages;
+      protobuf = protobuf3_0;
+    };
+in {
+  rysearch = stdenv.mkDerivation rec {
     name = "rysearch";
 
     buildInputs = [
@@ -11,6 +18,20 @@ with import <nixpkgs> {}; {
       python35Packages.scipy
       python35Packages.pandas
       python35Packages.scikitlearn
+      python35Packages.regex
+      python35Packages.virtualenv
     ];
+
+    shellHook = ''
+      if [ ! -d venv ]; then
+        virtualenv --python=python3.5 venv
+        venv/bin/pip install pymystem3
+        venv/bin/pip install tqdm
+        venv/bin/pip install protobuf==3.0.0
+      fi
+      export PATH="$(pwd)/venv/bin:$PATH"
+      export ARTM_SHARED_LIBRARY="$(pwd)/result/lib/libartm.so";
+      export PYTHONPATH="$PYTHONPATH:$(toPythonPath ${bigartm_python})";
+    '';
   };
 }
