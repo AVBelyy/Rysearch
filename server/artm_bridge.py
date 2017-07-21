000d12f55e5b56ca73dc1a504d0342a4a736afd5
Anton Belyy <anton.belyy@gmail.com>
2017-07-22 00:44:52 +0300

    Major back-end refactoring (split ARTM_bridge into ARTM_bridge and ARTM_lib, fix bugs, etc)

--- server/artm_bridge.py
+++ server/artm_bridge.py
@@ -7,18 +7,14 @@ import zmq
 import regex
 import os
 
-from sklearn.metrics.pairwise import pairwise_distances
-from pymongo import MongoClient
-from scipy.linalg import norm
+from parsers import arbitrary, text_utils
 from datetime import datetime
 
 import artm
-from experiments import hierarchy_utils
+import artm_lib
 
-from parsers import arbitrary, text_utils
 
 MODEL_PATH = "hartm"
-THETA_MODEL_PATH = "hartm.mdl"
 TRANSFORM_PATH = "uploads/transform.txt"
 BATCH_PATH = "uploads/transform_batches/"
 
@@ -26,210 +22,31 @@ ZMQ_BACKEND_PORT = 2511
 
 EMPTY, UP = b"", b"UP"
 
-EDGE_THRESHOLD = 0.05
-DOC_THRESHOLD = 0.25
-TOP_N_WORDS = 3
-TOP_N_REC_DOCS = 5
-TOP_N_REC_TAGS = 5
-TOP_N_TOPIC_DOCS = 20
 
-# List of all doc_id prefixes
-prefix_to_col_map = {"pn": "postnauka", "habr": "habrahabr",
-                     "elem": "elementy"}
-
-def hellinger_dist(p, q):
-    return norm(np.sqrt(p) - np.sqrt(q))
-
-def from_artm_tid(artm_tid):
-    # This is due to hARTM bug
-    if artm_tid.startswith("level_0_"):
-        return (0, artm_tid[8:])
-    else:
-        lid, tid = artm_tid[5:].split("_", 1)
-        lid = int(lid)
-        return (lid, tid)
-
-def to_artm_tid(lid, tid):
-    if lid < 0 or lid > len(Ts) or tid < 0 or tid >= sum(Ts):
-        return None
-
-    # This is due to hARTM bug
-    if lid == 0:
-        return "level_%d_topic_%d" % (lid, tid)
-    else:
-        return "level%d_topic_%d" % (lid, tid)
-
-def get_documents_by_ids(docs_ids, with_texts=True, with_modalities=False):
-    fields = {"title": 1, "authors_names" : 1}
-    if with_texts:
-        fields["markdown"] = 1
-    if with_modalities:
-        fields["modalities"] = 1
-    queries = {}
-    for doc_id in docs_ids:
-        prefix = doc_id.split("_", 1)[0]
-        col_name = prefix_to_col_map[prefix]
-        if col_name not in queries:
-            queries[col_name] = []
-        queries[col_name].append(doc_id)
-    result = []
-    for col_name, col_docs_ids in queries.items():
-        dataset = db["datasets"][col_name]
-        result += dataset.find({"_id": {"$in": col_docs_ids}}, fields)
-    result_map = dict(map(lambda v: (v["_id"], v), result))
-    response = []
-    for doc_id in docs_ids:
-        if doc_id not in result_map:
-            continue
-        doc = result_map[doc_id]
-        res = {
-            "doc_id":        doc["_id"],
-            "title":         doc["title"],
-            "authors_names": doc.get("authors_names", [])
-        }
-        if with_texts:
-            res["markdown"] = doc["markdown"]
-        if with_modalities:
-            res["modalities"] = doc["modalities"]
-        response.append(res)
-    return response
-
-# Initialize MongoDB
-db = MongoClient()
-
-# Initialize ARTM data
-artm_extra_info = pickle.load(open(MODEL_PATH + "/extra_info.dump", "rb"))
-artm_model = hierarchy_utils.hARTM(theta_columns_naming="title",
-                                   cache_theta=True,
-                                   class_ids=artm_extra_info["class_ids"])
-artm_model.load(MODEL_PATH)
-
-# Extract Phi, Psi and Theta matrices
-phis = []
-psis = []
-theta = artm_extra_info["theta"]
-# theta = pickle.load(open(THETA_MODEL_PATH, "rb"))["theta"]
-for level_idx, artm_level in enumerate(artm_model._levels):
-    phis.append(artm_level.get_phi(class_ids="flat_tag"))
-    if level_idx > 0:
-        psis.append(artm_level.get_psi())
-
-topics = {}
-T = lambda lid, tid: "level_%d_%s" % (lid, tid)
-unT = lambda t: list(map(int, t[6:].split("_topic_")))
-
-# Change this constants if model changes
-Ts = [20, 77]
-all_topics = theta.index
-rec_lid = 0
-rec_topics = list(filter(lambda t: regex.match("level_0_topic_*", t), all_topics))
-rec_theta = theta.T[rec_topics].sort_index()
-
-# Create subject topic names
-for lid, phi in enumerate(phis):
-    names = phi.index[phi.values.argsort(axis=0)[-2 * TOP_N_WORDS:][::-1].T]
-    for tid, top_words in zip(phi.columns, names):
-        # subject topic names are "topic_X", where X = 0, 1, ...
-        # background topic names are "background_X", where X = 0, 1, ...
-        if regex.match("^topic_\d+$", tid):
-            topics[T(lid, tid)] = {
-                "level_id":  lid,
-                "top_words": list(top_words),
-                "parents":   [],
-                "children":  [],
-                "weight":    0,
-            }
-
-# Collect topic edges
-for lid, psi in enumerate(psis):
-    density = 0
-    psi = psi > EDGE_THRESHOLD
-    for tid1 in psi.columns:
-        if regex.match("^topic_\d+$", tid1):
-            for tid2 in psi.index:
-                if regex.match("^topic_\d+$", tid2) and psi.loc[tid2, tid1]:
-                    density += 1
-                    topics[T(lid, tid1)]["children"].append(T(lid + 1, tid2))
-                    topics[T(lid + 1, tid2)]["parents"].append(T(lid, tid1))
-
-# Assign top words to child topics
-for topic_id, topic in topics.items():
-    used_top_words = sum(map(lambda tid: topics[tid]["top_words"][:TOP_N_WORDS],
-                             topic["parents"]), [])
-    topic["top_words"] = list(filter(lambda tw: tw not in used_top_words,
-                                     topic["top_words"]))[:TOP_N_WORDS]
-
-# Initialize doc thresholds
-doc_topics = list(filter(lambda t: regex.match("level1_topic_*", t), all_topics))
-doc_theta = theta.loc[doc_topics]
-doc_thresholds = doc_theta.max(axis=0) / np.sqrt(2)
-
-# Assign integer weights to topics
-topic_docs_count = doc_theta.apply(lambda s: sum(s >= doc_thresholds), axis=1)
-for artm_tid in doc_topics:
-    topic_id = T(*from_artm_tid(artm_tid))
-    w = int(topic_docs_count[artm_tid])
-    topics[topic_id]["weight"] = w
-for topic_id in topics:
-    if "weight" not in topics[topic_id]:
-        # TODO: fix when we have number of levels > 2
-        topics[topic_id]["weight"] = 0
-        for child_topic_id in topics[topic_id]["children"]:
-            topics[topic_id]["weight"] += topics[child_topic_id]["weight"]
+# Initialize ARTM bridge
+artm_bridge = artm_lib.ArtmBridge(MODEL_PATH)
 
 def process_msg(message):
     if message["act"] == "get_topics":
-        response = topics
+        response = artm_bridge.model.topics
     elif message["act"] == "get_documents":
-        lid, tid = unT(message["topic_id"])
-        artm_tid = to_artm_tid(lid, tid)
-        if artm_tid is None:
-            response = "Incorrect `topic_id`"
-        else:
-            ptd = doc_theta.loc[artm_tid]
-            sorted_ptd = ptd[ptd >= doc_thresholds].sort_values()
-            sorted_ptd = sorted_ptd[-TOP_N_TOPIC_DOCS:][::-1]
-            docs_ids = sorted_ptd.index
-            docs = get_documents_by_ids(docs_ids, with_texts=False)
-            weights = {k: float(v) for k, v in sorted_ptd.items()}
-            response = {"docs": docs, "weights": weights}
+        topic_id = message["topic_id"]
+        docs, weights = artm_bridge.get_documents_by_topic(topic_id, limit=20)
+        response = {"docs": docs, "weights": weights}
     elif message["act"] == "get_document":
         doc_id = message["doc_id"]
-        docs = get_documents_by_ids([doc_id], with_modalities=True)
+        docs = artm_bridge.data_source.get_documents_by_ids([doc_id], with_modalities=True)
         if len(docs) > 0:
             doc = docs[0]
             if message["recommend_tags"]:
-                # Tag recommendations
-                own_tags = set(doc["modalities"]["flat_tag"])
-                ptd = rec_theta.loc[doc_id]
-                topics_ids = list(map(lambda tid: from_artm_tid(tid)[1],
-                                      ptd.index))
-                weighted_tags = phis[rec_lid][topics_ids].mul(ptd.values)
-                rec_tags = {}
-                for _, pwt in weighted_tags.iteritems():
-                    top_tags = pwt.nlargest(len(own_tags) + TOP_N_REC_TAGS)
-                    for tag, w in top_tags.iteritems():
-                        tag = regex.sub("_", " ", tag)
-                        if tag not in own_tags:
-                            rec_tags[tag] = max(rec_tags.get(tag, 0), w)
-                rec_tags = list(map(lambda p: (p[1], p[0]), rec_tags.items()))
-                rec_tags.sort(reverse=True)
-                rec_tags = list(map(lambda x: x[1], rec_tags[:TOP_N_REC_TAGS]))
-                doc["recommended_tags"] = rec_tags
+                doc["recommended_tags"] = artm_bridge.recommend_tags_by_doc(doc)
             response = doc
         else:
             response = None
     elif message["act"] == "recommend_docs":
         doc_id = message["doc_id"]
-        if doc_id not in rec_theta.index:
-            response = "Unknown `doc_id`"
-        else:
-            doc = rec_theta.loc[doc_id]
-            dist = pairwise_distances([doc], rec_theta, hellinger_dist)[0]
-            dist_series = pd.Series(data=dist, index=rec_theta.index)
-            sim_docs_ids = dist_series.sort_values().index
-            sim_docs_ids = sim_docs_ids[1:TOP_N_REC_DOCS + 1]
-            response = get_documents_by_ids(sim_docs_ids, with_texts=False)
+        sim_docs_ids = artm_bridge.recommend_docs_by_doc(doc_id)
+        response = artm_bridge.data_source.get_documents_by_ids(sim_docs_ids, with_texts=False)
     elif message["act"] == "transform_doc":
         doc_path = message["doc_path"]
         with open(doc_path) as doc_file:
@@ -245,15 +62,18 @@ def process_msg(message):
                                                    data_path=TRANSFORM_PATH,
                                                    batch_size=1,
                                                    target_folder=BATCH_PATH)
-            transform_theta = artm_model.transform(transform_batch)
+            transform_theta = artm_bridge.model.transform(transform_batch)
             # Make a response
             response = {"theta": {}}
-            for artm_tid, prob in transform_theta["upload"].items():
-                topic_id = T(*from_artm_tid(artm_tid))
-                response["theta"][topic_id] = float(prob)
+            for artm_tid, pdt in transform_theta["upload"].items():
+                try:
+                    topic_id = artm_bridge.model.from_artm_tid(artm_tid)
+                    response["theta"][topic_id] = float(pdt)
+                except Exception as e:
+                    pass
         # Delete uploaded file
         os.remove(doc_path)
-    elif message["act"] == "get_next_assessment":
+    elif False and message["act"] == "get_next_assessment":
         ass_id = message["assessor_id"]
         ass_cnt = message["assessors_cnt"]
         col_name = message["collection_name"]
@@ -277,7 +97,7 @@ def process_msg(message):
             random.shuffle(unused_docs_ids)
             # Use batches of 100 docs per request
             response = unused_docs_ids[:100]
-    elif message["act"] == "assess_document":
+    elif False and message["act"] == "assess_document":
         doc_id = message["doc_id"]
         is_relevant = message["is_relevant"]
         col_names = [v for k, v in prefix_to_col_map.items()
---
+++ server/artm_lib.py
@@ -0,0 +1,269 @@
+import numpy as np
+import pandas as pd
+import pickle
+import regex
+import os
+
+from experiments import hierarchy_utils
+from pymongo import MongoClient
+from scipy.linalg import norm
+from sklearn.metrics.pairwise import pairwise_distances
+
+
+# List of all doc_id prefixes
+# TODO: move to config file
+prefix_to_col_map = {"pn": "postnauka", "habr": "habrahabr",
+                     "elem": "elementy"}
+
+
+def hellinger_dist(p, q):
+    return norm(np.sqrt(p) - np.sqrt(q))
+
+
+class ArtmModel:
+    def __init__(self, model_path, topic_naming_n_words=3, psi_edge_threshold=0.05):
+        # Load ARTM model and model extra info
+        self._extra_info = pickle.load(open(os.path.join(model_path, "extra_info.dump"), "rb"))
+        self._model = hierarchy_utils.hARTM(theta_columns_naming="title",
+                                            cache_theta=True,
+                                            class_ids=self._extra_info["class_ids"])
+        self._model.load(model_path)
+
+        # Extract Φ, Ψ, Ɵ matrices from model
+        self._phis = []
+        self._psis = []
+        self._theta = self._extra_info["theta"]
+        for level_idx, artm_level in enumerate(self._model):
+            self._phis.append(artm_level.get_phi(class_ids="flat_tag"))
+            if level_idx > 0:
+                self._psis.append(artm_level.get_psi())
+
+        # Construct topic name mappings
+        self._from_artm_tid_map = {}
+        self._to_artm_tid_map = {}
+        self._from_lid_tid_map = {}
+        self._to_lid_tid_map = {}
+        theta_new_index = []
+        for artm_tid in self._theta.index:
+            if artm_tid.startswith("level_0_"):
+                lid, tid = 0, artm_tid[8:]
+            else:
+                lid, tid = artm_tid[5:].split("_", 1)
+                lid = int(lid)
+            topic_id = "level_%d_%s" % (lid, tid) # internal project-consistent topic name
+            if tid.startswith("topic_"):
+                self._from_artm_tid_map[artm_tid] = topic_id
+                self._to_artm_tid_map[topic_id] = artm_tid
+                self._from_lid_tid_map[lid, tid] = topic_id
+                self._to_lid_tid_map[topic_id] = (lid, tid)
+            theta_new_index.append(topic_id)
+        self._theta.index = theta_new_index
+
+        # Construct topics infos
+        # TODO: make topic maning an external procedure
+        self._topics = {}
+        for lid, phi in enumerate(self._phis):
+            names = phi.index[phi.values.argsort(axis=0)[-2 * topic_naming_n_words:][::-1].T]
+            for tid, top_words in zip(phi.columns, names):
+                # subject topic names are "topic_X", where X = 0, 1, ...
+                # background topic names are "background_X", where X = 0, 1, ...
+                if regex.match("^topic_\d+$", tid):
+                    self._topics[self._from_lid_tid_map[lid, tid]] = {
+                        "level_id":  lid,
+                        "top_words": list(top_words),
+                        "parents":   [],
+                        "children":  [],
+                        "weight":    0,
+                    }
+
+        # Define parent-child relationship for topics
+        for lid, psi in enumerate(self._psis):
+            psi = (psi >= psi_edge_threshold)
+            for tid1 in psi.columns:
+                if regex.match("^topic_\d+$", tid1):
+                    for tid2 in psi.index:
+                        if regex.match("^topic_\d+$", tid2) and psi.loc[tid2, tid1]:
+                            topic_id_parent = self._from_lid_tid_map[lid, tid1]
+                            topic_id_child = self._from_lid_tid_map[lid + 1, tid2]
+                            self._topics[topic_id_parent]["children"].append(topic_id_child)
+                            self._topics[topic_id_child]["parents"].append(topic_id_parent)
+
+        # Assign top words to child topics
+        # TODO: make topic maning an external procedure
+        for topic_id, topic in self._topics.items():
+            used_top_words = sum(map(lambda tid: self._topics[tid]["top_words"][:topic_naming_n_words],
+                                     topic["parents"]), [])
+            topic["top_words"] = list(filter(lambda tw: tw not in used_top_words,
+                                             topic["top_words"]))[:topic_naming_n_words]
+
+        # Define parent-child relationship for topics and documents
+        last_lid = self.num_levels - 1
+        doc_topics = self.get_topics_ids_by_level(last_lid)
+        self._doc_theta = self._theta.loc[doc_topics]
+        self._doc_thresholds = self._doc_theta.max(axis=0) / np.sqrt(2)
+
+        # Define topic weight as:
+        # For last-level topic, weight = number of documents that belong to it
+        # For a higher-level topic, weight = sum(weight) of topic's child topics
+        docs_count = self._doc_theta.apply(lambda s: sum(s >= self._doc_thresholds), axis=1)
+        lids_tids = list(self._from_lid_tid_map.keys())
+        lids_tids = sorted(lids_tids, reverse=True)
+        for lid, tid in lids_tids:
+            topic_id = self._from_lid_tid_map[lid, tid]
+            if lid == last_lid:
+                w = int(docs_count[topic_id])
+            else:
+                w = 0
+                for child_topic_id in self._topics[topic_id]["children"]:
+                    w += self._topics[child_topic_id]["weight"]
+            self._topics[topic_id]["weight"] = w
+
+    def get_topics_ids_by_level(self, level_id):
+        if level_id < 0 or level_id >= self.num_levels:
+            raise ValueError("Unknown level_id: %d" % level_id)
+
+        topics_ids = []
+        for (lid, tid), topic_id in self._from_lid_tid_map.items():
+            if lid == level_id:
+                topics_ids.append(topic_id)
+        return topics_ids
+
+    def get_docs_ids_by_topic(self, topic_id):
+        if topic_id not in self._doc_theta.index:
+            raise ValueError("Unknown document topic id: '%s'" % topic_id)
+
+        ptd = self._doc_theta.loc[topic_id]
+        sorted_ptd = ptd[ptd >= self._doc_thresholds].sort_values(ascending=False)
+        return sorted_ptd
+
+    def transform(self, artm_batch):
+        return self._model.transform(artm_batch)
+
+    @property
+    def theta(self):
+        return self._theta
+
+    @property
+    def topics(self):
+        return self._topics
+
+    @property
+    def num_levels(self):
+        return self._model.num_levels
+
+    @property
+    def topics_ids(self):
+        return self._theta.index
+
+    def get_phi(self, level_id):
+        return self._phis[level_id]
+
+    def get_psi(self, level_id):
+        return self._psis[level_id]
+
+    def to_topic_id(self, lid, tid):
+        return self._from_lid_tid_map[lid, tid]
+
+    def from_topic_id(self, topic_id):
+        return self._to_lid_tid_map[topic_id]
+
+    def from_artm_tid(self, artm_tid):
+        return self._from_artm_tid_map[artm_tid]
+
+
+class ArtmDataSource:
+    def __init__(self):
+        self._db = MongoClient()
+
+    def get_documents_by_ids(self, docs_ids, with_texts=True, with_modalities=False):
+        fields = {"title": 1, "authors_names" : 1}
+        if with_texts:
+            fields["markdown"] = 1
+        if with_modalities:
+            fields["modalities"] = 1
+        queries = {}
+        for doc_id in docs_ids:
+            prefix = doc_id.split("_", 1)[0]
+            col_name = prefix_to_col_map[prefix]
+            if col_name not in queries:
+                queries[col_name] = []
+            queries[col_name].append(doc_id)
+        result = []
+        for col_name, col_docs_ids in queries.items():
+            dataset = self._db["datasets"][col_name]
+            result += dataset.find({"_id": {"$in": col_docs_ids}}, fields)
+        result_map = dict(map(lambda v: (v["_id"], v), result))
+        response = []
+        for doc_id in docs_ids:
+            if doc_id not in result_map:
+                continue
+            doc = result_map[doc_id]
+            res = {
+                "doc_id":        doc["_id"],
+                "title":         doc["title"],
+                "authors_names": doc.get("authors_names", [])
+            }
+            if with_texts:
+                res["markdown"] = doc["markdown"]
+            if with_modalities:
+                res["modalities"] = doc["modalities"]
+            response.append(res)
+        return response
+
+
+class ArtmBridge:
+    def __init__(self, model_path):
+        self._data_source = ArtmDataSource()
+        self._model = ArtmModel(model_path)
+
+        # Select topics which will be used for recommendation
+        self._rec_lid = 0
+        rec_topics = self._model.get_topics_ids_by_level(self._rec_lid)
+        self._rec_tids = list(map(lambda t: self._model.from_topic_id(t)[1], rec_topics))
+        self._rec_theta = self._model.theta.T[rec_topics].sort_index()
+
+    def get_documents_by_topic(self, topic_id, offset=0, limit=None, with_weights=True):
+        sorted_ptd = self._model.get_docs_ids_by_topic(topic_id)
+        if limit is None:
+            limit = len(sorted_ptd)
+
+        sorted_ptd = sorted_ptd[offset:offset + limit]
+        docs_ids = sorted_ptd.index
+        docs = self._data_source.get_documents_by_ids(docs_ids, with_texts=False)
+        weights = {k: float(v) for k, v in sorted_ptd.items()}
+
+        if with_weights:
+            return docs, weights
+        else:
+            return docs
+
+    def recommend_tags_by_doc(self, doc, rec_tags_count=5):
+        own_tags = set(doc["modalities"]["flat_tag"])
+        ptd = self._rec_theta.loc[doc["doc_id"]]
+        weighted_tags = self._model.get_phi(self._rec_lid)[self._rec_tids].mul(ptd.values)
+        rec_tags = {}
+        for _, pwt in weighted_tags.iteritems():
+            top_tags = pwt.nlargest(len(own_tags) + rec_tags_count)
+            for tag, w in top_tags.iteritems():
+                tag = regex.sub("_", " ", tag)
+                if tag not in own_tags:
+                    rec_tags[tag] = max(rec_tags.get(tag, 0), w)
+        rec_tags = list(map(lambda p: (p[1], p[0]), rec_tags.items()))
+        rec_tags.sort(reverse=True)
+        rec_tags = list(map(lambda x: x[1], rec_tags[:rec_tags_count]))
+        return rec_tags
+
+    def recommend_docs_by_doc(self, doc_id, rec_docs_count=5, metric=hellinger_dist):
+        doc = self._rec_theta.loc[doc_id]
+        dist = pairwise_distances([doc], self._rec_theta, hellinger_dist)[0]
+        dist_series = pd.Series(data=dist, index=self._rec_theta.index)
+        sim_docs_ids = dist_series.nsmallest(rec_docs_count + 1).index
+        return sim_docs_ids[1:] # Not counting the `doc` itself.
+
+    @property
+    def data_source(self):
+        return self._data_source
+
+    @property
+    def model(self):
+        return self._model
